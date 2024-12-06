f6cf1c504dd110da4aa3d6cbdceba0aac514327e
Bubl10k <vladislav.zagaiko@gmail.com>
2024-12-06 18:40:36 +0200

    Optimize notifications task

--- backend/apps/quiz/tasks.py
+++ backend/apps/quiz/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.db.models import Prefetch
 from django.utils.timezone import now
 
 from backend.apps.quiz.models import Quiz, Result
@@ -8,16 +9,24 @@ from backend.apps.users.models import CustomUser
 
 @shared_task
 def notify_users():
-    users = CustomUser.objects.all()
+    users = CustomUser.objects.all().prefetch_related(
+        Prefetch("company__quizzes", queryset=Quiz.objects.all(), to_attr="prefetched_quizzes"),
+        Prefetch(
+            "results", queryset=Result.objects.filter(status=Result.QuizStatus.COMPLETED), to_attr="prefetched_results"
+        ),
+    )
 
     for user in users:
-        for quiz in Quiz.objects.filter(company__members=user):
-            last_attempt = Result.objects.filter(user=user, quiz=quiz).order_by("-updated_at").first()
+        quizzes = getattr(user.company, "prefetched_quizzes", [])
+        user_results = {result.quiz.id: result for result in user.prefetched_results}
+
+        for quiz in quizzes:
+            last_attempt = user_results.get(quiz.id)
 
             if last_attempt and last_attempt.status == Result.QuizStatus.COMPLETED:
                 time_since_last_attempt = (now() - last_attempt.updated_at).days
             else:
                 time_since_last_attempt = float("inf")
 
-            if time_since_last_attempt > quiz.frequency:
+            if time_since_last_attempt >= quiz.frequency:
                 send_email_quiz_notification(user, quiz)
